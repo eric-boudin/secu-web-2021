aa40f8d5b984ff5afeff2d9da61970257907bb50
florian Naud <florian.naud254@gmail.com>
2021-10-19 22:44:27 +0200

    Upgrade sql injection show

--- SQLInjection/protected.php
+++ SQLInjection/protected.php
@@ -1,34 +1,44 @@
+<!doctype html>
+<html lang="en">
+    <head><title>Protection test</title></head>
+    <body>
+        <form><input type="text" name="xss"><input type="submit"></form>
+    </body>
+</html>
 <?php
-	$user = 'eric';
-	$password = 'persona5';
-	$mysqli = new mysqli("localhost", $user, $password, "test");
-	
-	//REQUETE
-	
-	echo("<br>Requete préparé");
-	$stmt = $mysqli->prepare('SELECT value FROM registry WHERE name= ?');
-	$stmt->bind_param("s", $name);
 
-	//ok
-	$name = 'eric';
-	echo('<br>param = '. $name);
-	$stmt->execute();
-	echo('<br>valeur recup :');
-	foreach ($stmt->get_result() as $row) {
-		echo $row['value'];
-		echo("\n");
-	}
-	
-	echo("<br>");
+	if(isset($_GET['xss'])) {
+		$user = 'eric';
+		$password = 'persona5';
+		$mysqli = new mysqli("localhost", $user, $password, "test");
+		
+		//REQUETE
+		
+		echo("<br>Requete préparé");
+		$stmt = $mysqli->prepare('SELECT value FROM registry WHERE name= ?');
+		$stmt->bind_param("s", $name);
 
-	//ko
-	$name = "pomme' or '1' = '1";
-	echo('<br>param = '. $name);
-	$stmt->execute();
-	echo('<br>valeur recup :');
-	foreach ($stmt->get_result() as $row) {
-		echo $row['value'];
-		echo("\n");
+		//ok
+		$name = $_GET['xss'];// 'eric'; || "pomme' or '1' = '1";
+		echo('<br>param = '. $name);
+		$stmt->execute();
+		echo('<br>valeur recup :');
+		foreach ($stmt->get_result() as $row) {
+			echo $row['value'];
+			echo("\n");
+		}
+		
+		/*echo("<br>");
+
+		//ko
+		$name = "pomme' or '1' = '1";
+		echo('<br>param = '. $name);
+		$stmt->execute();
+		echo('<br>valeur recup :');
+		foreach ($stmt->get_result() as $row) {
+			echo $row['value'];
+			echo("\n");
+		}*/
+		$stmt->close();
 	}
-	$stmt->close();
 ?>
--- SQLInjection/unprotected.php
+++ SQLInjection/unprotected.php
@@ -1,33 +1,43 @@
+<!doctype html>
+<html lang="en">
+    <head><title>Unprotection test</title></head>
+    <body>
+        <form><input type="text" name="xss"><input type="submit"></form>
+    </body>
+</html>
 <?php
-	$user = 'eric';
-	$password = 'persona5';
-	$mysqli = new mysqli("localhost", $user, $password, "test");
-	
-	//QUERY
-	
-	//ok
-	$name = "eric";
-	echo('<br>param = '. $name);
-	$sql = 'SELECT value FROM registry WHERE name=\''.$name.'\'' ;
-	echo('<br>sql = '. $sql);
-	echo('<br>valeur recup :');
-	$res = $mysqli->query($sql);
-	foreach($res as $row) {
-        echo $row['value'];
-		echo("\n");
-    }
-	
-	echo("<br>");
-	
-	//kos
-	$name = "pomme' or '1' = '1";
-	echo('<br>param = '. $name);
-	$sql = 'SELECT value FROM registry WHERE name=\''.$name.'\'' ;
-	echo('<br>sql = '. $sql);
-	echo('<br>valeur recup :');
-	$res = $mysqli->query($sql);
-	foreach($res as $row) {
-        echo $row['value'];
-		echo("\n");
-    }
+
+	if(isset($_GET['xss'])) {
+		$user = 'eric';
+		$password = 'persona5';
+		$mysqli = new mysqli("localhost", $user, $password, "test");
+		
+		//QUERY
+		
+		//ok
+		$name = $_GET['xss']; //"eric"; || "pomme' or '1' = '1";
+		echo('<br>param = '. $name);
+		$sql = 'SELECT value FROM registry WHERE name=\''.$name.'\'' ;
+		echo('<br>sql = '. $sql);
+		echo('<br>valeur recup :');
+		$res = $mysqli->query($sql);
+		foreach($res as $row) {
+			echo $row['value'];
+			echo("\n");
+		}
+		
+		/*echo("<br>");
+		
+		//kos
+		$name = "pomme' or '1' = '1";
+		echo('<br>param = '. $name);
+		$sql = 'SELECT value FROM registry WHERE name=\''.$name.'\'' ;
+		echo('<br>sql = '. $sql);
+		echo('<br>valeur recup :');
+		$res = $mysqli->query($sql);
+		foreach($res as $row) {
+			echo $row['value'];
+			echo("\n");
+		}*/
+	}
 ?>
